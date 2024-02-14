c777ae3a29c46d0d8015cf04790ae645b4065978
Mindaugas Laskis <Mindaugas.Laskis@bentley.com>
2024-02-14 14:56:17 +0200

    Fix mobile UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,17 +5,16 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Jorė portfolio</title>
+        <title>Jorė Gritėnaitė</title>
         <meta
           name="description"
           content="A portfolio website for showcasing audio art"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen max-w-screen-lg flex-col justify-between bg-white px-16 py-10">
-        <div className="flex flex-row justify-between gap-12">
+      <main className="min-h-screen max-w-screen-lg grid grid-cols-1 md:grid-cols-2 bg-white px-16 py-10 gap-8 place-content-between">
           <div className="text-2xl">Jorė Gritėnaitė</div>
-          <div className="flex flex-col gap-4 items-start">
+          <div className="flex flex-col gap-8 items-start">
             <AudioButton
               file="/Mouthwash.mp3"
               name="to create the sense of intimacy when there’s none, 2023"
@@ -23,11 +22,8 @@ export default function Home() {
             <AudioButton file="/Mouthwash.mp3" name="Mouthwash, 2023" />
             <AudioButton file="/hearing_you.mp3" name="Hearing You Is Nothing Like Reading You, 2023" />
           </div>
-        </div>
-        <div className="flex flex-row gap-24 text-xl">
           <div>contact</div>
           <div>about</div>
-        </div>
       </main>
     </>
   );
