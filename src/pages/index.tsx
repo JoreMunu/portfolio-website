5040c738874965b347b9260f34207a03b2933251
Mindaugas Laskis <Mindaugas.Laskis@bentley.com>
2024-02-21 19:51:06 +0200

    Add missing audio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,13 +16,22 @@ export default function Home() {
         <div className="text-2xl">Jorė Gritėnaitė</div>
         <div className="flex flex-col items-start gap-8">
           <AudioButton
-            file="/Mouthwash.mp3"
-            name="to create the sense of intimacy when there’s none, 2023"
+            file="/Hearing_You_Is_Nothing_Like_Reading_You.mp3"
+            name="Hearing You Is Nothing Like Reading You, 2023"
           />
           <AudioButton file="/Mouthwash.mp3" name="Mouthwash, 2023" />
           <AudioButton
-            file="/hearing_you.mp3"
-            name="Hearing You Is Nothing Like Reading You, 2023"
+            file="/to_create_the_sense_of_intimacy_when_there_s_none.mp3"
+            name="to create the sense of intimacy when there’s none, 2023"
+          />
+          <AudioButton
+            file="/spring_sound_collection_2023.mp3"
+            name="spring sound collection 2023, 2023"
+          />
+          <AudioButton file="/Babygirl.mp3" name="Babygirl, 2022" />
+          <AudioButton
+            file="/Paralyzing_Your_Tongue.mp3"
+            name="Paralyzing Your Tongue, 2022"
           />
         </div>
         <div>contact</div>
