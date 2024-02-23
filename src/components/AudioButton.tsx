f22cc159a685df962bf0a755dde2ae87ae5e05eb
Mindaugas Laskis <Mindaugas.Laskis@bentley.com>
2024-02-23 19:13:07 +0200

    Implement audio links

--- src/components/AudioButton.tsx
+++ src/components/AudioButton.tsx
@@ -1,14 +1,31 @@
-import useAudio from "~/hooks/useAudio";
+import { useEffect, useRef } from "react";
 
-export function AudioButton({ file, name }: { file: string; name: string }) {
-  const [playing, toggle] = useAudio(file);
+export function AudioButton({
+  file,
+  name,
+  play,
+  onClick,
+}: {
+  file: string;
+  name: string;
+  play: boolean;
+  onClick: () => void;
+}) {
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  useEffect(() => {
+    if (audioRef.current) {
+      play ? void audioRef.current.play() : void audioRef.current.pause();
+    }
+  }, [play]);
 
   return (
     <button
-      className={`text-start text-4xl italic transition hover:opacity-80 ${playing ? "text-gray-500" : "text-black"}`}
-      onClick={toggle}
+      className={`text-start text-4xl italic transition hover:opacity-80 ${play ? "text-gray-500" : "text-black"}`}
+      onClick={onClick}
     >
       {name}
+      <audio ref={audioRef} src={file} />
     </button>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,54 @@
 import Head from "next/head";
 import { AudioButton } from "~/components/AudioButton";
 import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+const audioItems: {
+  file: string;
+  name: string;
+}[] = [
+  {
+    file: "Hearing_You_Is_Nothing_Like_Reading_You.mp3",
+    name: "Hearing You Is Nothing Like Reading You, 2023",
+  },
+  { file: "Mouthwash.mp3", name: "Mouthwash, 2023" },
+  {
+    file: "to_create_the_sense_of_intimacy_when_there_s_none.mp3",
+    name: "to create the sense of intimacy when there’s none, 2023",
+  },
+  {
+    file: "spring_sound_collection_2023.mp3",
+    name: "spring sound collection 2023, 2023",
+  },
+  { file: "Babygirl.mp3", name: "Babygirl, 2022" },
+  { file: "Paralyzing_Your_Tongue.mp3", name: "Paralyzing Your Tongue, 2022" },
+];
 
 export default function Home() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const audioToPlay = searchParams.get("audio");
+  const [interacted, setInteracted] = useState(false);
+
+  useEffect(() => {
+    const setInteractedTrue = () => {
+      setInteracted(true);
+    };
+
+    // Add event listeners for the 'click' and 'keydown' events
+    window.addEventListener("click", setInteractedTrue);
+    window.addEventListener("keydown", setInteractedTrue);
+
+    // Clean up function
+    return () => {
+      window.removeEventListener("click", setInteractedTrue);
+      window.removeEventListener("keydown", setInteractedTrue);
+    };
+  }, [searchParams]);
+
   return (
     <>
       <Head>
@@ -16,24 +62,25 @@ export default function Home() {
       <main className="grid min-h-screen max-w-screen-lg grid-cols-1 place-content-between gap-8 bg-white px-16 py-10 md:grid-cols-2">
         <div className="text-2xl">Jorė Gritėnaitė</div>
         <div className="flex flex-col items-start gap-8">
-          <AudioButton
-            file="/Hearing_You_Is_Nothing_Like_Reading_You.mp3"
-            name="Hearing You Is Nothing Like Reading You, 2023"
-          />
-          <AudioButton file="/Mouthwash.mp3" name="Mouthwash, 2023" />
-          <AudioButton
-            file="/to_create_the_sense_of_intimacy_when_there_s_none.mp3"
-            name="to create the sense of intimacy when there’s none, 2023"
-          />
-          <AudioButton
-            file="/spring_sound_collection_2023.mp3"
-            name="spring sound collection 2023, 2023"
-          />
-          <AudioButton file="/Babygirl.mp3" name="Babygirl, 2022" />
-          <AudioButton
-            file="/Paralyzing_Your_Tongue.mp3"
-            name="Paralyzing Your Tongue, 2022"
-          />
+          {audioItems.map((item) => (
+            <AudioButton
+              key={item.file}
+              file={`/${item.file}`}
+              name={item.name}
+              play={interacted && item.file === audioToPlay}
+              onClick={() =>
+                item.file === audioToPlay
+                  ? router.push("/")
+                  : router.push(
+                      { query: { ...router.query, audio: item.file } },
+                      undefined,
+                      {
+                        shallow: true,
+                      },
+                    )
+              }
+            />
+          ))}
         </div>
         <Link
           className="underline transition hover:opacity-80"
